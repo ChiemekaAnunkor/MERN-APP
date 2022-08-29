39f01a020b886c65d8c192aa3c461a5b2b36d98d
ChiemekaAnunkor <anunkorjr@gmail.com>
2022-08-29 15:01:29 -0500

    'update'

--- server/index.js
+++ server/index.js
@@ -1,28 +1,40 @@
+//framework for creating the routing of our applicaiton
+import express from "express";
+//enables us to send post requests
+import bodyParser from "body-parser";
+//enables modles for the post
+import mongoose from "mongoose";
+//enable cross orgina request (i.e server to client)
+import cors from "cors";
+import dotenv from "dotenv";
 
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import dotenv from "dotenv"
-
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
-dotenv.config()
+dotenv.config();
 
-app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+//set limits for our requests
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+
 app.use(cors());
 
-app.use('/posts', postRoutes);
+//default url is localhost/post 
+app.use("/posts", postRoutes);
 
-const CONNECTION_URL = process.env.CONNECTION_URL
+const CONNECTION_URL = process.env.CONNECTION_URL;
 
-;
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+// newurlparser and useunfied avoids errors that may arrise due to depreciation of things
+//setup connection with catch
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
   .catch((error) => console.log(`${error} did not connect`));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -1,21 +1,22 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const postSchema = mongoose.Schema({
-    title: String,
-    message: String,
-    creator: String,
-    tags: [String],
-    selectedFile: String,
-    likeCount: {
-        type: Number,
-        default: 0,
-    },
-    createdAt: {
-        type: Date,
-        default: new Date(),
-    },
-})
+  title: String,
+  message: String,
+  creator: String,
+  tags: [String],
+  selectedFile: String,
+  likeCount: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+});
 
-var PostMessage = mongoose.model('PostMessage', postSchema);
+var PostMessage = mongoose.model("PostMessage", postSchema);
 
+//allows name imports with out curly bracts
 export default PostMessage;
